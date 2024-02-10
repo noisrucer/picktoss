9e2ad016def054dd1d0b5b652f8c55ca2bb19ffe
changiinlee <changjin9792@gmail.com>
2024-02-11 00:56:25 +0900

    changed oauth redirect url to localhost temporarily

--- reminder/domain/member/model.py
+++ reminder/domain/member/model.py
@@ -1,4 +1,5 @@
-from sqlalchemy import BigInteger, String
+from datetime import datetime
+from sqlalchemy import BigInteger, String, Boolean, DateTime
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 from reminder.core.database.session_manager import Base
@@ -11,6 +12,12 @@ class Member(Base, AuditBase):
     id: Mapped[str] = mapped_column(String(100), primary_key=True, nullable=False, index=True)
     name: Mapped[str] = mapped_column(String(500), nullable=False)
     email: Mapped[str] = mapped_column(String(500), nullable=False)
+    # pro_version: Mapped[bool] = mapped_column(Boolean, default=False, nullable=False)
+    # pro_version_purchased_at: Mapped[datetime] = mapped_column(DateTime, nullable=True)
+
+    # def is_pro_version(self) -> bool:
+    #     return self.pro_version
+    
 
     ## -- relationships
     categories = relationship("Category", back_populates="member", cascade="all, delete-orphan")
